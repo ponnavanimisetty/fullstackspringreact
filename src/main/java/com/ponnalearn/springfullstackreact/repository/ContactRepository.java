052c186844647a58b4416aaee64f9b199bab0288
Ponnaga Vanimisetti <Ponnaga_Vanimisetti@epam.com>
2019-08-22 12:28:30 +0530

    Completed calling add functionality

--- src/main/java/com/ponnalearn/springfullstackreact/repository/ContactRepository.java
+++ src/main/java/com/ponnalearn/springfullstackreact/repository/ContactRepository.java
@@ -3,9 +3,11 @@ package com.ponnalearn.springfullstackreact.repository;
 
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.ponnalearn.springfullstackreact.model.Contact;
 
+@RepositoryRestResource
 public interface ContactRepository extends CrudRepository<Contact, Long> {
 
 	
